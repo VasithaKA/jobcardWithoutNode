46f79abd2aaa4c40851d2f9ced760d0cfceae2e1
Vasitha <VasithaKA@users.noreply.github.com>
2019-02-12 22:37:59 +0530

    server

--- api/routes/relation_routes/solves.js
+++ api/routes/relation_routes/solves.js
@@ -53,7 +53,7 @@ router.post('/', async (req, res) => {
 
 //complete the job
 router.patch('/setComplete/:jobId', async (req, res) => {
-    await Solve.findOneAndUpdate({ jobId: req.params.jobId }, { $set: { status: "complete" } })
+    await Solve.findOneAndUpdate({ jobId: req.params.jobId }, { $set: { status: "complete", endTime: moment().format() } })
         .then(() => {
             res.json({
                 success: "success"
